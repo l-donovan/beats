1f55089e6ea3556abdeb64dd8854810bf20634e9
Luke Donovan <ladbuilder1@gmail.com>
2016-06-13 09:50:50 -0400

    changed key/button binding process

--- beats.py
+++ beats.py
@@ -9,13 +9,15 @@ import threading
 CHUNK = 2**10
 p = pyaudio.PyAudio()
 
-clips = {
-	'a': "audio/Wet_Dreamz_Loop_Kick.wav",
-	's': "audio/Wet_Dreamz_Loop_Snare.wav",
-	'c': "audio/Wet_Dreamz_Loop_Strings_3.wav",
-	'x': "audio/Wet_Dreamz_Loop_Strings_2.wav",
-	'z': "audio/Wet_Dreamz_Loop_Strings_1.wav"
-	}
+clips = [
+	"audio/Wet_Dreamz_Loop_Kick.wav",
+	"audio/Wet_Dreamz_Loop_Snare.wav",
+	"audio/Wet_Dreamz_Loop_Strings_3.wav",
+	"audio/Wet_Dreamz_Loop_Strings_2.wav",
+	"audio/Wet_Dreamz_Loop_Strings_1.wav"
+	]
+
+clipsKeys = {}
 
 reset = False
 playThread = None
@@ -32,6 +34,7 @@ def playClipThread(path):
 	while (data and not reset):
 		stream.write(data)
 		data = wf.readframes(CHUNK)
+
 def killThread():
 	global reset
 	global playThread
@@ -53,20 +56,38 @@ def keydown(e):
 	if (e.char == ' '):
 		killThread()
 		return
-	clip = clips.get(e.char, False)
-	if (clip): playClip(clip)
-	
+	clipId = clipsKeys.get(e.char, -1)
+	if (clipId != -1): playClip(clips[clipId])
+
 root = Tk()
 frame = Frame(root, width=100, height=100)
 frame.bind("<KeyPress>", keydown)
 frame.focus_set()
 
-buttons = [None] * len(clips)
-for i, key in enumerate(clips):
-	buttons[i] = Button(root, text=key, anchor=W, justify=LEFT, padx=2, command=lambda j=key+'': playClip(clips[j]))
-	buttons[i].config(activeforeground="green")
-	buttons[i].config(disabledforeground="white")
-	buttons[i].pack()
+buttons = [None] * 16
+for i in range(len(buttons)):
+	buttons[i] = Button(frame, text='', width=2, height=2)
+	buttons[i].grid(row=i // 4, column=i % 4)
+
+def assignButton(i, clipId):
+	buttons[i].config(command=lambda j=clipId+0: playClip(clips[clipId]))
+
+def assignKey(char, clipId):
+	clipsKeys[char] = clipId
+
+# KEY ASSIGNMENTS
+assignKey('a', 0)
+assignKey('s', 1)
+assignKey('c', 2)
+assignKey('x', 3)
+assignKey('z', 4)
+
+# BUTTON ASSIGNMENTS
+assignButton(0, 0)
+assignButton(1, 1)
+assignButton(4, 4)
+assignButton(5, 3)
+assignButton(6, 2)
 
 frame.pack()
 root.mainloop()
