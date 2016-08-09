e6d4592e46f3e6f8a7f554b4dbaebee0b9484c46
Luke Donovan <ladbuilder1@gmail.com>
2016-08-09 17:44:44 -0400

    changed default pad

--- pad.py
+++ pad.py
@@ -1,25 +1,16 @@
 {
     "clips": [
-        "audio/Wet_Dreamz_Loop_Kick.wav",
-        "audio/Wet_Dreamz_Loop_Snare.wav",
-        "audio/Wet_Dreamz_Loop_Strings_3.wav",
-        "audio/Wet_Dreamz_Loop_Strings_2.wav",
-        "audio/Wet_Dreamz_Loop_Strings_1.wav"
+        "audio/OhMyGodKick.wav",
+        "audio/OhMyGodSnare.wav"
     ],
 
     "keyMap": {
         'a': 0,
-        's': 1,
-        'c': 2,
-        'x': 3,
-        'z': 4
+        's': 1
     },
 
     "buttonMap": {
         0: 0,
-        1: 1,
-        4: 4,
-        5: 3,
-        6: 2
+        1: 1
     }
 }
